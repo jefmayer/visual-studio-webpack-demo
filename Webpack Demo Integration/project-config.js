21ad329eba839f34632dea5b7a465ea21d60c552
Jef Mayer <jef.a.mayer@gmail.com>
2025-02-12 15:30:34 -0700

    removing hard-coded directories and files from webpack.config, moving them to project-config.js. updating webpack dependencies

---
+++ Webpack Demo Integration/project-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  paths: {
+    dirs: {
+      src: {
+        scss: './src/scss',
+        js: './src/js',
+      },
+      dist: {
+        css: './wwwroot/dist/css',
+        js: './wwwroot/dist/js'
+      }
+    },
+    files: {
+      src: {
+        scss: {
+          styles: './src/scss/styles.scss',
+        },
+        js: {
+          layout: './src/js/layout.js',
+          modules: './src/js/modules.js',
+        },
+      },
+    },
+  },
+};
--- Webpack Demo Integration/webpack.config.js
+++ Webpack Demo Integration/webpack.config.js
@@ -1,31 +1,76 @@
+const config = require('./project-config');
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
+class CreateDirectoryPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.beforeRun.tapAsync('CreateDirectoryPlugin', (compilation, callback) => {
+      this.options.directories.forEach((dir) => {
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true });
+        }
+      });
+      callback();
+    });
+  }
+}
+
+class RemoveFilePlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.done.tap('CustomTaskPlugin', () => {
+      const files = this.options.files;
+      Object.keys(files).forEach(key => {
+        try {
+          const file = files[key].match(/\/([^\/]+)\.scss$/)[1];
+          const assetPath = path.resolve(__dirname, `${config.paths.dirs.dist.css}/${file}.js`);
+          if (fs.existsSync(assetPath)) {
+            fs.unlinkSync(assetPath);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      })
+    });
+  }
+}
+
 module.exports = [
   {
     mode: 'production',
-    entry: {
-      layout: './src/js/layout.js',
-      modules: './src/js/modules.js',
-    },
+    entry: config.paths.files.src.js,
     output: {
       filename: '[name].min.js',
-      path: path.resolve(__dirname, 'wwwroot/js'),
+      path: path.resolve(__dirname, config.paths.dirs.dist.js),
       clean: true,
       sourceMapFilename: '[name].min.js.map',
     },
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.jsx?$/,
           exclude: /node_modules/,
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+              ],
             },
           },
         },
@@ -38,17 +83,26 @@ module.exports = [
       ],
     },
     plugins: [
-      new ESLintPlugin({ extensions: ['js'] }),
+      new ESLintPlugin({ extensions: ['js', 'jsx'] }),
+      new CreateDirectoryPlugin({
+        directories: [
+          path.resolve(__dirname, config.paths.dirs.dist.css),
+          path.resolve(__dirname, config.paths.dirs.dist.js)
+        ]
+      })
     ],
     devtool: 'source-map',
   },
   {
     mode: 'production',
-    entry: {
-      styles: './src/scss/styles.scss',
-    },
+    entry: config.paths.files.src.scss,
+    ignoreWarnings: [
+      (warning) => {
+        return warning.message.includes('Deprecation Warning') || warning.message.includes('Module Warning');
+      },
+    ],
     output: {
-      path: path.resolve(__dirname, 'wwwroot/css'),
+      path: path.resolve(__dirname, config.paths.dirs.dist.css),
     },
     module: {
       rules: [
@@ -56,7 +110,7 @@ module.exports = [
           test: /\.s[ac]ss$/i,
           use: [
             MiniCssExtractPlugin.loader,
-            { loader: "css-loader", options: { sourceMap: true } },
+            { loader: "css-loader", options: { sourceMap: true, url: false } },
             'postcss-loader',
             { loader: "sass-loader", options: { sourceMap: true } },
           ],
@@ -65,7 +119,8 @@ module.exports = [
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '[name].min.css' }),
-      new StylelintPlugin({ files: 'src/scss/**/*.{scss,sass,css}' }),
+      new StylelintPlugin({ files: `${config.paths.dirs.src.scss}/**/*.{scss,sass,css}` }),
+      new RemoveFilePlugin({ files: config.paths.files.src.scss }), 
     ],
     devtool: 'source-map',
   }
